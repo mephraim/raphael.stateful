3eece3f61b723ce65ed20f860fae7fc6009e9406
Matthew Ephraim <mephraim@matthew-ephraims-computer.local>
2009-11-10 20:18:06 -0600

    Added before and after handlers for the states and for state switching

--- demo/demo.js
+++ demo/demo.js
@@ -2,12 +2,29 @@ window.onload = function() {
   var paper = Raphael(0,0, 800, 600);
   var rect = paper.rect(20, 20, 100, 100);
   
-  rect.addState('blah', { 
+  rect.addState('bigger', { 
     attrs: { 
-      height: 100 
+      height: 300,
+      width: 300,
+      strokeWeight: 500,
+      fill: '#3C9CD3'
     }
   });
   
-  rect.state('blah');
+  rect.addState('smaller', {
+    attrs: {
+      height: 100,
+      width: 100,
+      fill: '#FFE563',
+      stroke: '#F7FF00'
+    }
+  });
+  
+  rect.state('bigger', { 
+    time: 1000,
+    after: function() {
+      rect.state('smaller', { time: 1000 });
+    }
+  });
 }
 
--- spec/raphael.stateful_spec.js
+++ spec/raphael.stateful_spec.js
@@ -14,6 +14,10 @@ Screw.Unit(function() {
     it("adds a getState function to Raphael elements", function() {
       expect(jQuery.isFunction(rect.getState)).to(equal, true);
     });
+    
+    it("adds a state function to Raphael elements", function() {
+      expect(jQuery.isFunction(rect.state)).to(equal, true);
+    });
   });
   
   describe("the addState function", function() {
@@ -31,6 +35,162 @@ Screw.Unit(function() {
       rect.addState('test', state);
 
       expect(rect.getState('test')).to(equal, state);
+    });
+  });
+  
+  describe("the state function", function() {
+    it("it switches the attributes of the element to match the attributes for the state", function() {
+      rect.addState('test', { 
+        attrs: { height: 100 }
+      });
+      
+      rect.attr({ height: 0 });
+      rect.state('test');
+      expect(rect.attr('height')).to(equal, 100);
+    });
+        
+    it("animates with the right speed if a time is passed in", function() {
+      var animationSpeed;
+      rect.animate = function(attr, speed) {
+        animationSpeed = speed;
+      }
+      
+      rect.addState('test', {}); 
+      rect.state('test', { time: 5000 });
+      
+      expect(animationSpeed).to(equal, 5000);
+    });
+    
+    describe("before callbacks", function() {
+      it("executes the before function that was passed in before switching states", function() {
+        var stateSwitched = false;
+        var beforeCalled = false;
+
+        rect.attr = function() {
+          stateSwitched = true;
+        };
+
+        rect.addState('test', {});
+
+        rect.state('test', {
+          before: function() {
+            if (!stateSwitched) {
+              beforeCalled = true;
+            }
+          }
+        });
+
+        expect(beforeCalled).to(equal, true);
+      });
+
+      it("executes the before function that is part of the state before switching states", function() {
+        var stateSwitched = false;
+        var beforeCalled = false;
+
+        rect.attr = function() {
+          stateSwitched = true;
+        };
+
+        rect.addState('test', {
+          before: function() {
+            if (!stateSwitched) {
+              beforeCalled = true;
+            }
+          }
+        });
+        rect.state('test', {});
+
+        expect(beforeCalled).to(equal, true);
+      });
+    });
+    
+    describe("after callbacks", function() {
+      it("executes the after function that was passed in after switching states normally", function() {
+        var stateSwitched = false;
+        var afterCalled = false;
+
+        rect.attr = function() {
+          stateSwitched = true;
+        };
+
+        rect.addState('test', {});
+        rect.state('test', {
+          after: function() {
+            if (stateSwitched) {
+              afterCalled = true;
+            }
+          }
+        });
+
+        expect(afterCalled).to(equal, true);
+      });
+
+      it("executes the after function that was passed in after switching states with animation", function() {
+        var stateSwitched = false;
+        var afterCalled = false;
+
+        rect.animate = function(attrs, time, callback) {
+          if (callback) {
+            stateSwitched = true;
+          }
+          callback.apply(this);
+        };
+
+        rect.addState('test', {});
+        rect.state('test', {
+          time: 2,
+          after: function() {
+            if (stateSwitched) {
+              afterCalled = true;
+            }
+          }
+        });
+
+        expect(afterCalled).to(equal, true);
+      });
+
+      it("executes the after function that is part of the state after switching states normally", function() {
+        var stateSwitched = false;
+        var afterCalled = false;
+
+        rect.attr = function() {
+          stateSwitched = true;
+        };
+
+        rect.addState('test', {
+          after: function() {
+            if (stateSwitched) {
+              afterCalled = true;
+            }
+          }
+        });
+
+        rect.state('test');
+        expect(afterCalled).to(equal, true);
+      });
+      
+      it("executes the after function that is part of the state after switching states with animation", function() {
+        var stateSwitched = false;
+        var afterCalled = false;
+
+        rect.animate = function(attrs, time, callback) {
+          if (callback) {
+            stateSwitched = true;
+          }
+          callback.apply(this);
+        };
+
+        rect.addState('test', {
+          after: function() {
+            if (stateSwitched) {
+              afterCalled = true;
+            }
+          }
+        });
+        rect.state('test', { time: 2 });
+
+        expect(afterCalled).to(equal, true);
+      });
     });
   });
 });
